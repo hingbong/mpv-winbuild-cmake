b134f0d21b432638e568747e608161b9b117a30f
Hingbong Lo <hingbonglo@gmail.com>
2023-04-13 20:12:45 +0800

    ffmpeg use cyanreg/FFmpeg/vulkan

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -38,7 +38,8 @@ ExternalProject_Add(ffmpeg
         vapoursynth
         uavs3d
         davs2
-    GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
+    GIT_REPOSITORY https://github.com/cyanreg/FFmpeg.git
+    GIT_TAG vulkan
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
